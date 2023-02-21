19b1a2b40ada0243dec65c17a47a3e1e2a8fbcaa
anujkumar98 <kumar.anuj@northeastern.edu>
2023-02-20 21:04:46 -0500

    Added fast-api

---
+++ src/fastapi/database_util.py
@@ -0,0 +1,110 @@
+import sqlite3
+import json
+
+class database_methods():
+    def __init__(self):
+        conn = sqlite3.connect('src/data/USER_DATA.db')
+        self.cursor_user = conn.cursor()
+        self.cursor_user.execute('''CREATE TABLE IF NOT EXISTS USER (id INTEGER PRIMARY KEY AUTOINCREMENT ,username TEXT NOT NULL, password TEXT NOT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP)''')
+        conn_geo = sqlite3.connect('src/data/GEOSPATIAL_DATA.db')
+        conn.commit()
+        self.cursor_geo = conn_geo.cursor()
+        
+    def create_connection(self,database_name):
+        conn = sqlite3.connect(f'src/data/{database_name}.db')
+        cursor = conn.cursor()
+        return conn,cursor
+        
+    def to_dict_list(self,rows, columns):
+        return [dict(zip(columns, row)) for row in rows]
+
+    def add_user(self,username,password):
+        try:
+            conn,cursor_user=self.create_connection('USER_DATA')
+            cursor_user.execute("INSERT INTO USER (username, password) VALUES (?,?)", (username,password))
+            conn.commit()
+            conn.close()
+            return "user_created"
+        except Exception as e:
+            print("add_user: "+str(e))
+            return "failed_insert"
+        
+    
+    def fetch_user(self,user_name):
+        try:
+            _,cursor_user=self.create_connection('USER_DATA')
+            query=f"select * from USER where username='{user_name}'"
+            cursor_user.execute(query)
+            rows = cursor_user.fetchall()
+            if len(rows)==0:
+                return "no_user_found"
+            else:
+                print(self.return_json(rows,cursor_user))
+                return self.return_json(rows,cursor_user)
+        except Exception as e:
+            return 'Exception'
+
+    
+    def return_json (self, rows,cursor):
+        result = []
+        for row in rows:
+            d = dict(zip([col[0] for col in cursor.description], row))
+            result.append(d)
+        # Convert the list of dictionaries to a JSON string
+        json_result = json.dumps(result)
+        json_obj = json.loads(json_result)
+        # Print the JSON string
+        return(json_obj)
+
+    #### Query for GOES ####
+    
+    def geos_get_year(self):
+        _,cursor_geo=self.create_connection('GEOSPATIAL_DATA')
+        cursor_geo.execute('SELECT DISTINCT year from goes18meta')
+        rows = cursor_geo.fetchall()
+        return self.return_json(rows,cursor_geo)
+
+    def geos_get_day(self,year):
+        _,cursor_geo=self.create_connection('GEOSPATIAL_DATA')
+        cursor_geo.execute(f'SELECT DISTINCT day from goes18meta where year={year}')
+        rows=cursor_geo.fetchall()
+        return self.return_json(rows,cursor_geo)
+
+    def geos_get_hour(self,year, day):
+        _,cursor_geo=self.create_connection('GEOSPATIAL_DATA')
+        cursor_geo.execute(f'SELECT DISTINCT hour from goes18meta where year={year} and day={day}')
+        rows=cursor_geo.fetchall()
+        return self.return_json(rows,cursor_geo)
+    
+    #### Query for NEXRAD ####
+    def nexrad_get_year(self):
+        _,cursor_geo=self.create_connection('GEOSPATIAL_DATA')
+        cursor_geo.execute(f'SELECT DISTINCT year from nexradmetadata')
+        rows=cursor_geo.fetchall()
+        return self.return_json(rows,cursor_geo)
+
+    def nexrad_get_month(self,year):
+        _,cursor_geo=self.create_connection('GEOSPATIAL_DATA')
+        cursor_geo.execute(f'SELECT DISTINCT month from nexradmetadata where year={year}')
+        rows=cursor_geo.fetchall()
+        return self.return_json(rows,cursor_geo)
+
+    def nexrad_get_day(self,year, month):
+        _,cursor_geo=self.create_connection('GEOSPATIAL_DATA')
+        cursor_geo.execute(f'SELECT DISTINCT day from nexradmetadata where year={year} and month={month}')
+        rows=cursor_geo.fetchall()
+        return self.return_json(rows,cursor_geo)
+
+    def nexrad_get_sites(self,year, month, day):
+        _,cursor_geo=self.create_connection('GEOSPATIAL_DATA')
+        cursor_geo.execute(f'SELECT DISTINCT stationcode from nexradmetadata where year={year} and month={month} and day={day}')
+        rows=cursor_geo.fetchall()
+        return self.return_json(rows,cursor_geo)
+
+    #nexrad sites
+
+    def get_nexrad_sites(self):
+        _,cursor_geo=self.create_connection('GEOSPATIAL_DATA')
+        cursor_geo.execute(f'SELECT lat, lon from nexrad_sites_data')
+        rows=cursor_geo.fetchall()
+        return self.return_json(rows,cursor_geo)
---
+++ src/fastapi/main.py
@@ -0,0 +1,96 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime, timedelta
+from typing import Union
+from fastapi import Depends, FastAPI, HTTPException, status
+from .user_auth import AuthHandler
+from .database_util import database_methods
+from .aws_s3_copy import s3_copy
+
+app = FastAPI()
+auth_handler = AuthHandler()
+users = []
+db_method=database_methods()
+copy_obj=s3_copy()
+
+class UserInput(BaseModel):
+    year:int
+    month:int
+    date:int
+    station:Optional [str] = None
+    
+class UserData(BaseModel):
+    username:str
+    password: str
+
+
+@app.get("/fetch_url_nexrad",status_code=status.HTTP_200_OK)
+async def fetch_url(userinput: UserInput,username=Depends(auth_handler.auth_wrapper)):
+    aws_nexrad_url = f"https://noaa-nexrad-level2.s3.amazonaws.com/index.html#{userinput.year:04}/{userinput.month:02}/{userinput.date:02}/{userinput.station}"
+    return {'url': aws_nexrad_url }
+
+@app.get("/fetch_url_goes",status_code=status.HTTP_200_OK)
+async def fetch_url(userinput: UserInput,username=Depends(auth_handler.auth_wrapper)):
+    aws_nexrad_url = f"https://noaa-goes18.s3.amazonaws.com/index.html#ABI-L1b-RadC/{userinput.year:04}/{userinput.month:02}/{userinput.date:02}"
+    return {'url': aws_nexrad_url }
+
+
+
+@app.post('/register', status_code=status.HTTP_201_CREATED)
+def register(auth_details: UserData):
+    user_fetch_status=db_method.fetch_user(auth_details.username)
+    if user_fetch_status != 'no_user_found' or user_fetch_status == 'Exception': 
+        raise HTTPException(status_code=400, detail='Username is taken')
+    hashed_password = auth_handler.get_password_hash(auth_details.password)
+    user_status=db_method.add_user(auth_details.username,hashed_password)
+    if user_status=='failed_insert':
+        raise HTTPException(status_code=400, detail='Error')
+
+@app.post('/login',status_code=status.HTTP_200_OK)
+def login(auth_details: UserData):
+    fetch_user_status=db_method.fetch_user(auth_details.username)
+    if (fetch_user_status == 'no_user_found' or not auth_handler.verify_password(auth_details.password, fetch_user_status[0]['password'])):
+        raise HTTPException(status_code=401, detail='Invalid username and/or password')
+    token = auth_handler.encode_token(fetch_user_status[0]['username'])
+    return { 'token': token }
+
+
+@app.get('/geos_get_year',status_code=status.HTTP_200_OK)
+def goes_year(username=Depends(auth_handler.auth_wrapper)):
+    return db_method.geos_get_year()
+
+@app.get('/geos_get_day/{year}',status_code=status.HTTP_200_OK)
+def goes_year(year:int,username=Depends(auth_handler.auth_wrapper)):
+    return db_method.geos_get_day(year)
+
+@app.get('/geos_get_hour/{year}/{day}',status_code=status.HTTP_200_OK)
+def goes_year(year:int, day:int,username=Depends(auth_handler.auth_wrapper)):
+    return db_method.geos_get_hour(year, day)
+
+@app.get('/nexrad_get_year',status_code=status.HTTP_200_OK)
+def goes_year(username=Depends(auth_handler.auth_wrapper)):
+    return db_method.nexrad_get_year()
+
+@app.get('/nexrad_get_month/{year}',status_code=status.HTTP_200_OK)
+def goes_year(year:int,username=Depends(auth_handler.auth_wrapper)):
+    return db_method.nexrad_get_month(year)
+
+
+@app.get('/nexrad_get_day/{year}/{month}',status_code=status.HTTP_200_OK)
+def goes_year(year:int, month:int,username=Depends(auth_handler.auth_wrapper)):
+    return db_method.nexrad_get_day(year, month)
+
+@app.get('/nexrad_get_sites/{year}/{month}/{day}',status_code=status.HTTP_200_OK)
+def goes_year(year:int, month:int, day:int,username=Depends(auth_handler.auth_wrapper)):
+    return db_method.nexrad_get_sites(year, month, day)
+
+@app.get('/get_nexrad_sites',status_code=status.HTTP_200_OK)
+def goes_year(username=Depends(auth_handler.auth_wrapper)):
+    return db_method.get_nexrad_sites()
+
+@app.get('/copy_file_s3/{source_bucket_name}/{key}',status_code=status.HTTP_200_OK)
+def copy_file_s3(source_bucket_name:str,key:str):
+    status_copy=copy_obj.copy_file_into_s3(source_bucket_name,key)
+    if status_copy == False:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Invalid request')
